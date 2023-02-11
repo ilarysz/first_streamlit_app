8fd54d4c69a9b07f46902bd4be3d8e80097347a3
Ireneusz Larysz <39213764+ilarysz@users.noreply.github.com>
2023-02-11 20:51:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 
 streamlit.title("Streamlit App - Dinner Menu")
@@ -31,6 +32,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Show dataframe 
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
 
 # Snowflake connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
